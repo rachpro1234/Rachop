08475803b1460477ebb4acec5a2ea82046e6dad7
Rachid El Barqouqy <108005388+rachpro1234@users.noreply.github.com>
2024-09-15 12:34:18 +0100

    n.config file modified

--- next.config.mjs
+++ next.config.mjs
@@ -9,14 +9,13 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin(
   // specify a custom path for the i18n
   "./src/i18n.ts"
-  
 );
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
-
 export default withNextIntl(nextConfig);
-
