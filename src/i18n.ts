92b495297e98ab8cf6515c7f87f7ad8afaa08df5
Rachid El Barqouqy <108005388+rachpro1234@users.noreply.github.com>
2026-06-21 14:43:33 +0100

    updated the i18n file and upgraded deprecated code

--- src/i18n.ts
+++ src/i18n.ts
@@ -4,15 +4,18 @@ import { getRequestConfig } from "next-intl/server";
 // Can be imported from a shared config
 const locales = ["en", "de", "it"];
 
-export default getRequestConfig(async ({ locale }) => {
+export default getRequestConfig(async ({ requestLocale }) => {
+
+  const locale = await requestLocale;
   // define the basename of the language file
-  const baseLocaleName = new Intl.Locale(locale).baseName;
+  const baseLocaleName = new Intl.Locale(locale ?? "en").baseName;
 
   // Validate that the incoming `locale` parameter is valid
   if (!locales.includes(baseLocaleName)) notFound();
   // if(!locales.includes(locale as any)) notFound();
 
   return {
+    locale: baseLocaleName,
     messages: (await import(`./messages/${baseLocaleName}.json`)).default,
   };
 });
