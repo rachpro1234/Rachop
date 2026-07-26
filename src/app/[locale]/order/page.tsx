290eeec0f669c5fc5d63d9f654970f87ae4a2f33
Rachid El Barqouqy <108005388+rachpro1234@users.noreply.github.com>
2026-07-26 16:15:17 +0100

    customized the order page and optimized the overall layout

--- src/app/[locale]/order/page.tsx
+++ src/app/[locale]/order/page.tsx
@@ -8,6 +8,7 @@ import { AppDispatch, useAppSelector } from "../redux/store";
 
 import { updateCart } from "../redux/features/cart-slice";
 import Stars from "../components/Stars";
+import { motion } from "motion/react";
 
 interface cartItems {
   id: number;
@@ -26,72 +27,6 @@ const Product: React.FC = () => {
   const dispatch = useDispatch<AppDispatch>();
   const cartArray: cartItems[] = useAppSelector((state) => state.cartReducer);
 
-  // const temptCartItems: cartItems[] = [
-
-  //   {
-  //     id: 0,
-  //     img: "/products/product-1.jpg",
-  //     title: `${t("jacket")}`,
-  //     desc: `${t("greyman_jacket_heliko_tex")}`,
-  //     price: 45,
-  //     prevPrice: 95,
-  //     quantity: 1,
-  //   },
-  //   {
-  //     id: 1,
-  //     img: "/products/product-2.jpg",
-  //     title: `${t("skirt")}`,
-  //     desc: `${t("brown_floral_wrap_midi_skirt")}`,
-  //     price: 55,
-  //     prevPrice: 105,
-  //     quantity: 1,
-  //   },
-  //   {
-  //     id: 2,
-  //     img: "/products/product-3.jpg",
-  //     title: ` ${t("party_wear")}`,
-  //     desc: `${t("women_party_shoes")}`,
-  //     price: 25,
-  //     prevPrice: 75,
-  //     quantity: 1,
-  //   },
-  //   {
-  //     id: 3,
-  //     img: "/products/product-4.jpg",
-  //     title: `${t("shirt")}`,
-  //     desc: `${t("men_corporate_shirt")}`,
-  //     price: 45,
-  //     prevPrice: 95,
-  //     quantity: 1,
-  //   },
-  //   {
-  //     id: 4,
-  //     img: "/products/product-5.jpg",
-  //     title: `${t("shoes")}`,
-  //     desc: `${t("green_waterproof_hiking_shoes")}`,
-  //     price: 100,
-  //     prevPrice: 107,
-  //     quantity: 1,
-  //   },
-  //   {
-  //     id: 5,
-  //     img: "/products/product-6.jpg",
-  //     title: `${t("watches")}`,
-  //     desc: `${t("smart_watches_vital_plus")}`,
-  //     price: 100,
-  //     prevPrice: 150,
-  //     quantity: 1,
-  //   },
-  //   {
-  //     id: 6,
-  //     img: "/products/product-7.jpg",
-  //     title: `${t("watches")}`,
-  //     desc: `${t("pocket_watch_leather_pouch")}`,
-  //     price: 120,
-  //     prevPrice: 170,
-  //     quantity: 1,
-  //   },
-  // ];
 
   useEffect(() => {
     setCartItems(cartArray);
@@ -140,51 +75,61 @@ const Product: React.FC = () => {
           {t("Cart_is_empty_please_add_an_item")}
         </h1>
       ) : null}
-      <div className="flex flex-col gap-4">
+      <div className="flex flex-col gap-4 max-w-[1280px] mx-auto">
         {cartItems.map((item, index) => {
           return (
-            <div
+            <motion.div
+              initial={{ opacity: 0, y: 20 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.5, delay: index * 0.1 }}
+              viewport={{ once: true }}
               key={index}
-              className="md:flex md:justify-around flex  items-center gap-4 border-blue-500 border-separate"
+              className="flex gap-4 border-blue-500 border-separate"
             >
               <div className="product-img">
                 <Image src={item.img} width={200} height={200} alt="image" className="rounded-lg"/>
               </div>
-              <div className="md:flex gap-4 justify-center items-center">
-                <h3 className="text-accent font-bold uppercase">
-                  {item.title}
-                </h3>
-                <p className="text-[#aaa] max-w-[200px] capitalize">
-                  {item.desc}
-                </p>
-                <span>
-                  <Stars currentRating={null} />
-                </span>
-                <h2 className="font-bold dark:text-white">{item.price * item.quantity}$</h2>
-                <div className="flex items-center gap-4">
-                  <button
-                    type="button"
-                    onClick={() => decrementCartItems(index)}
-                    className="bg-purple-600 p-2 rounded-full w-[21px] h-[21px] flex justify-center items-center text-[#fff]"
-                  >
-                    -
-                  </button>
-                  <span className="dark:text-white">{item.quantity}</span>
-                  <button
-                    type="button"
-                    onClick={() => incrementCartItems(index)}
-                    className="bg-purple-600 p-2 rounded-full w-[21px] h-[21px] flex justify-center items-center text-[#fff]"
-                  >
-                    +
-                  </button>
+              <div className="flex flex-col justify-between">
+                <div>
+                  <h3 className="text-accent font-bold uppercase text-3xl">
+                    {item.title}
+                  </h3>
+                  <p className="text-[#aaa] max-w-[200px] capitalize text-lg">
+                    {item.desc}
+                  </p>
+                  <span>
+                    <Stars currentRating={null} />
+                  </span>
+                  <h2 className="font-bold dark:text-white text-4xl">{item.price * item.quantity}$</h2>
+                  <div className="flex items-center gap-4 w-fit rounded-xl border-black border-2 border-solid px-2">
+                    <button
+                      type="button"
+                      onClick={() => decrementCartItems(index)}
+                      className="bg-accent p-2 rounded-full w-[21px] h-[21px] flex justify-center items-center text-white"
+                    >
+                      -
+                    </button>
+                    <span className="dark:text-white">{item.quantity}</span>
+                    <button
+                      type="button"
+                      onClick={() => incrementCartItems(index)}
+                      className="bg-accent p-2 rounded-full w-[21px] h-[21px] flex justify-center items-center text-[#fff]"
+                    >
+                      +
+                    </button>
+                  </div>
+                </div>
+                <span 
+                className="flex gap-2 text-xl cursor-pointer uppercase hover:underline hover:text-accent transition-all duration-300 ease-in-out"
+                onClick={() => removeCartItems(index)}>
+                  delete
                   <Trash
-                    size={32}
-                    onClick={() => removeCartItems(index)}
+                    size={26}
                     className="cursor-pointer hover:text-accent dark:text-white"
                   />
-                </div>
+                </span>
               </div>
-            </div>
+            </motion.div>
           );
         })}
       </div>
